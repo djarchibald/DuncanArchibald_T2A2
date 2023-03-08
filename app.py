ebcd930a03d64222380bd33d26a5d9cfc951fbaa
djarchibald <13827@coderacademy.edu.au>
2023-03-08 13:53:05 +1100

    user added, db link established

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#establish connection                       dbms                db_user     pwd     URI     PORT    db_name
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://std2_db_dev:224245@localhost:5432/T2A2_Lens_Library"
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
