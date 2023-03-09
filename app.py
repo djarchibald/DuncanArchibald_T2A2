08eac27b384fbd0d15388b74928833bdd5e957ba
djarchibald <13827@coderacademy.edu.au>
2023-03-09 12:57:34 +1100

    schemas and get lens/lenses

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 #CONFIG
 
@@ -8,6 +9,7 @@ app = Flask(__name__)
 #establish connection                       dbms                db_user     pwd     URI     PORT    db_name
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://std2_db_dev:224245@localhost:5432/T2A2_Lens_Library"
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 #MODELS
 class Lens (db.Model):
@@ -21,6 +23,16 @@ class Lens (db.Model):
         mount = db.Column(db.String())
         max_aperture = db.Column(db.Varchar(3))
 
+#SCHEMAS
+
+class LensSchema(ma.Schema)
+    class Meta:
+         fields = ("lens_id", "model", "manufacturer", "mount", "max_aperture")
+#multiple lenses schema to handle list of lenses
+lenses_schema = LensSchema(many=True)    
+#single lens schema to handle lens object 
+lens_schema = LensSchema()
+         
 
 #CLI COMMANDS
 
@@ -40,7 +52,15 @@ def seed_db():
 def hello_world():
     return "<p>Hello, World!</p>"
 
+#lists all lenses
 @app.route("/lenses", methods =["GET"])
 def get_lenses():
      lens_list = Lens.query.all()
-     return jsonify(lens_list)
+     data = lenses_schema.dump(lens_list)
+     return data
+#single lens found by lens_id
+@app.route("/lenses/<int:id>", methods=["GET"])
+def get_lens(id):
+    lens = Lens.query.get(id)
+    data = lens_schema.dump(lens)
+    return data
