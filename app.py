3f841d30d4fab258ce4d3db0742694ce1060dbf3
djarchibald <13827@coderacademy.edu.au>
2023-03-16 12:15:09 +1100

    lens post route added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+
 #CONFIG
 
 app = Flask(__name__)
@@ -25,13 +26,13 @@ class Lens (db.Model):
 
 #SCHEMAS
 
-#class LensSchema(ma.Schema):
-    #class Meta:
-     #    fields = ("lens_id", "model", "manufacturer", "mount", "max_aperture")
+class LensSchema(ma.Schema):
+    class Meta:
+        fields = ("lens_id", "model", "manufacturer", "mount", "max_aperture")
 #multiple lenses schema to handle list of lenses
-#lenses_schema = LensSchema(many=True)    
+lenses_schema = LensSchema(many=True)    
 #single lens schema to handle lens object 
-#lens_schema = LensSchema()
+lens_schema = LensSchema()
          
 
 #CLI COMMANDS
@@ -60,7 +61,7 @@ def seed_db():
     db.session.add(lens2)
 
     lens3 = Lens()
-    lens3.model = "RF 15-35mm f/2.8L IS USM"
+    lens3.model = "RF 15-35 f/2.8L IS USM"
     lens3.manufacturer = "Canon"
     lens3.mount = "RF"
     lens3.max_aperture = "2.8"
@@ -68,7 +69,7 @@ def seed_db():
     db.session.add(lens3)
 
     lens4 = Lens()
-    lens4.model = "10-24mm f/3.5-4.5 Di II VC HLD"
+    lens4.model = "10-24 f/3.5-4.5 Di II VC HLD"
     lens4.manufacturer = "Tamron"
     lens4.mount = "EF"
     lens4.max_aperture = "3.5"
@@ -76,7 +77,7 @@ def seed_db():
     db.session.add(lens4)
 
     lens5 = Lens()
-    lens5.model = "AF 85mm F1.4 DG HSM Art"
+    lens5.model = "AF 85 f1.4 DG HSM Art"
     lens5.manufacturer = "Sigma"
     lens5.mount = "EF"
     lens5.max_aperture = "1.4"
@@ -84,7 +85,7 @@ def seed_db():
     db.session.add(lens5)
 
     lens6 = Lens()
-    lens6.model = "XF16-55mm F2.8 R LM WR"
+    lens6.model = "XF16-55 f2.8 R LM WR"
     lens6.manufacturer = "Fujinon"
     lens6.mount = "X"
     lens6.max_aperture = "2.8"
@@ -92,7 +93,7 @@ def seed_db():
     db.session.add(lens6)
 
     lens7 = Lens()
-    lens7.model = "FE 24-70mm f/2.8 G Master"
+    lens7.model = "FE 24-70 f/2.8 G Master"
     lens7.manufacturer = "Sony"
     lens7.mount = "FE"
     lens7.max_aperture = "2.8"
@@ -100,7 +101,7 @@ def seed_db():
     db.session.add(lens7)
 
     lens8 = Lens()
-    lens8.model = "24-70mm f/2.8 DG DN Art"
+    lens8.model = "24-70 f/2.8 DG DN Art"
     lens8.manufacturer = "Sigma"
     lens8.mount = "EF"
     lens8.max_aperture = "2.8"
@@ -108,7 +109,7 @@ def seed_db():
     db.session.add(lens8)
 
     lens9 = Lens()
-    lens9.model = "FE 50mm f/1.4 GM"
+    lens9.model = "FE 50 f/1.4 GM"
     lens9.manufacturer = "Sony"
     lens9.mount = "FE"
     lens9.max_aperture = "1.4"
@@ -116,7 +117,7 @@ def seed_db():
     db.session.add(lens9)
 
     lens10 = Lens()
-    lens10.model = "Super-Elmar-M 18mm f/3.8 ASPH"
+    lens10.model = "Super-Elmar-M 18 f/3.8 ASPH"
     lens10.manufacturer = "Leica"
     lens10.mount = "M"
     lens10.max_aperture = "3.8"
@@ -158,34 +159,49 @@ def drop_db():
     
 #ROUTES
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "<p>Welcome to the Lens Library!</p>"
 
 #lists all lenses
-#@app.route("/lenses", methods =["GET"])
-#def get_lenses():
-#     lens_list = Lens.query.all()
-#    data = lenses_schema.dump(lens_list)
-#     return data
-#single lens found by lens_id
-#@app.route("/lenses/<int:id>", methods=["GET"])
-#def get_lens(id):
-#    lens = Lens.query.get(id)
-#    data = lens_schema.dump(lens)
-#    return data
+@app.route("/lenses", methods =["GET"])
+def get_lenses():
+    lens_list = Lens.query.all()
+    data = lenses_schema.dump(lens_list)
+    return data
 
-#@app.route("/books", methods= ["POST"])
-#def post_lens():
+#single lens found by lens_id
+@app.route("/lenses/<int:id>", methods=["GET"])
+def get_lens(id):
+    lens = Lens.query.get(id)
+    data = lens_schema.dump(lens)
+    return data
+
+
+@app.route("/lenses", methods= ["POST"])
+def post_lens():
+    #receive lens fields from the flas request, use schema to load
+    lens_fields = lens_schema.load(request.json)
+    #accsessing lens fields
+
+    lens = Lens(
+         model = lens_fields["model"],
+         manufacturer = lens_fields["manufacturer"],
+         mount = lens_fields["mount"],
+         max_aperture = lens_fields["max_aperture"]
+    )
+
+    db.session.add(lens)
+    db.session.commit()
+    return lens_schema.dump(lens)
 #Lens fields received from request (from flask) & use schema to load.
 
-#    lens_fields = lens_schema.load(request.json)
-#    lens = Lens(
-#          model = lens_fields["model"],
-#          manufacturer = lens_fields["manufacturer"],
-#          mount = lens_fields["mount"],
-#          max_aperture = lens_fields["max_aperture"]
-#    )
+    # lens_fields = lens_schema.load(request.json)
+    # lens = Lens(
+    #       model = lens_fields["model"],
+    #       manufacturer = lens_fields["manufacturer"],
+    #       mount = lens_fields["mount"],
+    #       max_aperture = lens_fields["max_aperture"]
+    #)
 
-#db.session.add(lens)
-#db.session.commit()
-#return lens_schema.dump(lens)
+    
+
